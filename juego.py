ca0128c52e6306788c2b5a479d872547a8ea7d51
n3al2006 <programingN3AL@gmail.com>
2025-04-12 19:38:17 -0500

    Update juego.py

--- juego.py
+++ juego.py
@@ -1,1 +1,40 @@
+# Importamos la libreria random
+import random
 
+# Posibles casos de maquina y lo que escribas
+turnoMaquina = ["Piedra", "Papel", "Tijera"]
+turnoHumano = ["Piedra", "Papel", "Tijera"]
+
+while True:
+    num = random.randrange(0,3)
+    print("================== Bienvenido al juego de Piedra | Papel | Tijera ==============")
+    print()
+    a = input("Escribe Piedra, Papel o Tijera: ")
+    print(f"Maquina dice: {turnoMaquina[num].lower()}")
+    
+    # Aqui hacemos las validaciones a posibles casos donde ganaría la maquina
+    
+    if turnoMaquina[num].lower() == turnoMaquina[0].lower() and a.lower() == turnoHumano[2].lower():
+        print("========== MAQUINA GANA =============")
+        print("")
+        break
+    
+    elif turnoMaquina[num].lower() == turnoMaquina[1].lower() and a.lower() == turnoHumano[0].lower():
+        print("========== MAQUINA GANA =============")
+        print("")
+        break
+        
+    elif turnoMaquina[num].lower() == turnoMaquina[2].lower() and a.lower() == turnoHumano[1].lower():
+        print("========== MAQUINA GANA =============")
+        print("")
+        break
+        
+    # Validación a posible empate
+    elif turnoMaquina[num].lower() == a.lower() :
+        print("========== EMPATE =============")
+        break
+    
+    else:
+        print("============ GANASTE ==============")
+        break
+        
